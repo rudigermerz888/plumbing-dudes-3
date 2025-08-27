270252ac82e9fec1e54ff0c464fa72da6878ced8
rudigermerz888 <rudiger@growingwithcredit.com>
2025-08-27 08:56:28 -0700

    Update astro.config.mjs
    Removed the invalid serverless configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
+  adapter: vercel(),
   site: 'https://ramonsplumbing.com',
   base: '/',
   trailingSlash: 'ignore',
